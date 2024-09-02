7cf6fe1f84a8d56b484efaec489e73ef97f31b17
Sophie Houben <mariesophie.houben@gmail.com>
2024-09-02 17:35:45 +0200

    Print review title, text, and rating for static HTML page

--- scraper/main.py
+++ scraper/main.py
@@ -1,1 +1,20 @@
-print("Hello world!")
+from bs4 import BeautifulSoup
+import pandas as pd
+import json
+
+file_path = '/Users/sophiehouben/insurance-customer-reviews/scraper/example-reviews.html'
+
+with open(file_path, 'r', encoding='utf-8') as file:
+    html_content = file.read()
+
+soup = BeautifulSoup(html_content, 'html.parser')
+
+json_content = soup.find('script', type='application/ld+json').string.strip()
+parsed_content = json.loads(json_content)['@graph']
+
+for element in parsed_content:
+    if element['@type'] == 'Review':
+        print(element['reviewRating']['ratingValue'])
+        print(element['headline'])
+        print(element['reviewBody'])
+        print("_____")
