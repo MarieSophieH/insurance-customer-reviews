1760a02e4f9ca695af263fcf7027e3eba4d36e97
Sophie Houben <mariesophie.houben@gmail.com>
2024-09-02 19:49:13 +0200

    Cover multiple insurances

--- scraper/main.py
+++ scraper/main.py
@@ -4,33 +4,44 @@ import parser
 import csv
 import requests
 
-is_avalaible = True
-index = 1
-url = 'https://www.trustpilot.com/review/huk-coburg.de?languages=all'
+insurances = {
+    'AOK': 'www.aok.de',
+    'HUK': 'www.huk-coburg.de',
+    'LV1871': 'www.lv1871.de',
+    'Allianz': 'www.allianz.de' 
+}
 
-with open('insurance_ratings.csv', mode='w', newline='', encoding='utf-8') as file:
-    writer = csv.writer(file, delimiter=';')
+for insurance in insurances:
 
-    writer.writerow(['Headline', 'Text', 'Rating'])
+    insurance_url = insurances[insurance]
+    url = 'https://www.trustpilot.com/review/' + insurance_url + '?languages=all'
+    filename = 'ratings_' + insurance + '.csv'
+    with open(filename, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file, delimiter=';')
 
-    while(is_avalaible):
-        request_url = url
+        writer.writerow(['Headline', 'Text', 'Rating'])
 
-        if index > 1:
-            request_url += '&page=' + str(index)
-            
-        index += 1
-        response = requests.get(request_url)
+        is_avalaible = True
+        index = 1
 
-        if response.status_code == 200:
-            html_content = response.text
-            parsed_content = parser.parseHtmlContent(html_content)
+        while(is_avalaible):
+            request_url = url
 
-            for element in parsed_content:
-                headline = element['headline']
-                text = element['text']
-                rating = element['rating']
-                writer.writerow([headline, text, rating])
+            if index > 1:
+                request_url += '&page=' + str(index)
+                
+            index += 1
+            response = requests.get(request_url)
 
-        elif response.status_code == 404:
-            is_avalaible = False
+            if response.status_code == 200:
+                html_content = response.text
+                parsed_content = parser.parseHtmlContent(html_content)
+
+                for element in parsed_content:
+                    headline = element['headline']
+                    text = element['text']
+                    rating = element['rating']
+                    writer.writerow([headline, text, rating])
+
+            elif response.status_code == 404:
+                is_avalaible = False
